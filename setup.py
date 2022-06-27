4cea1b9e97aa6df584dd750681355b6b3acabf95
zhangzhen0302 <Zhangzhen0302468>
2022-06-27 18:27:52 +0800

    easy353 1.5.1 修改打包程序

--- setup.py
+++ setup.py
@@ -25,23 +25,39 @@ from setuptools import setup, find_packages
 script_to_install = ["easy353.py", "src/build_database.py", "script/alignment.py", "script/combine.py",
                      "script/compare.py", "script/statistics.py", "script/tree.py"]
 
+# python libs
+install_dependencies = []
+try:
+    import biopython
+except ImportError:
+    install_dependencies.append("bipython>=1.70")
+else:
+    sys.stdout.write("Existed module numpy " + str(biopython.__version__) + "\n")
 
-def readme():
-    """Get README.md content."""
-    with open("README.md", 'r') as f:
-        return f.read()
-
-
-def license_():
-    """Get LICENSE.txt content."""
-    with open("LICENSE", 'r') as f:
-        return f.read()
-
-
-def find_requirements():
-    """Read requirements.txt file and returns list of requirements."""
-    with open("requirements.txt", 'r') as f:
-        return f.read().splitlines()
+try:
+    import psutil
+except ImportError:
+    install_dependencies.append("psutil>=5.6.6")
+else:
+    sys.stdout.write("Existed module sympy " + str(psutil.__version__) + "\n")
+try:
+    import requests
+except ImportError:
+    install_dependencies.append("requests[security]")
+else:
+    sys.stdout.write("Existed module requests " + str(requests.__version__) + "\n")
+try:
+    from bs4 import beautifulsoup4
+except ImportError:
+    install_dependencies.append("beautifulsoup4>=4.9.0")
+else:
+    sys.stdout.write("Existed module beautifulsoup4 " + str(beautifulsoup4.__version__) + "\n")
+try:
+    import tqdm
+except ImportError:
+    install_dependencies.append("tqdm>=4.31.1")
+else:
+    sys.stdout.write("Existed module tqdm " + str(tqdm.__version__) + "\n")
 
 
 sys.stdout.write("Python " + str(sys.version).replace("\n", " ") + "\n")
@@ -50,18 +66,17 @@ sys.stdout.write("Using setuptools " + str(setuptools.__version__) + "\n")
 
 setup(
     name="Easy353",
-    version="v1.5.0",
+    version="1.5.0",
     author="zzhen",
     author_email="zzhen0302@gmail.com",
     description="""a tool for recovering the Angiosperms353 gene set(AGS), 
                    which can filter and de novo assemble reads from sequencing data,
                    helping users capture AGS accurately and effectively.""",
-    long_description=readme(),
-    license=license_(),
+    license="MIT License",
     # 项目主页
     url="https://github.com/plant720/Easy353",
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     packages=find_packages(),
-    install_requires=find_requirements(),
+    install_requires=install_dependencies,
     scripts=script_to_install,
 )
